657f8c70a151ac26d0d90fdc6fc1d34982594096
EhogGawlic <ethang0227@gmail.com>
2025-10-24 21:51:04 +0000

    six seven (6 - 7)

--- engine.js
+++ engine.js
@@ -31,6 +31,7 @@ class Shape{
                 this.bi = buffer.boxes.length-1
                 break
             case 'ball':
+                this.anchored=false
                 buffer.addBall(this.sz,this.pos[0],this.pos[1],this.pos[2],this.color.r,this.color.g,this.color.b)
                 this.ei = buffer.ind+0
                 this.bi =buffer.balls.length-1
@@ -44,13 +45,49 @@ class Shape{
             case 'ball':
                 this.buf.moveBallTo(this.bi,x,y,z)
         }
+        this.buf.updateBuffers()
         this.pos = [x,y,z]
     }
     update(){
-        if (!this.anchored){
-            
+        if (true){
+            this.pos =[
+                this.pos[0]+this.v[0],
+                this.pos[1]+this.v[1],
+                this.pos[2]+this.v[2]
+            ]
+            this.v[1]+=0.01
+            this.moveTo(this.pos[0],this.pos[1],this.pos[2])
+            //collision?
         }
+        
     }
+}
+function getDescendants(obj) {
+  const result = [];
+  
+  // A helper function to perform the recursive traversal
+  function traverse(currentObj) {
+    for (const key in currentObj) {
+      if (Object.prototype.hasOwnProperty.call(currentObj, key)) {
+        const value = currentObj[key];
+        
+        // Add the current property to the result array
+        if(typeof value == Shape){
+            result.push(value)
+        }
+        
+        // Check if the value is a non-null object and not an array
+        if (typeof value === 'object' && value !== null && !Array.isArray(value) && typeof value != Shape) {
+          // If it's an object, recurse into it
+          traverse(value);
+        }
+      }
+    }
+  }
+  
+  // Start the traversal with the initial object
+  traverse(obj);
+  return result;
 }
 export class Game{
     things={}
@@ -76,14 +113,20 @@ export class Game{
         const box = new Shape("box",this.buffer,[sx,sy,sz],color)
         box.moveTo(x,y,z)
         parent[name]=box
+        return parent[name]
     }
     addBall(x,y,z,rad,parent,name,color){
         const ball = new Shape("ball",this.buffer,rad,color)
         ball.moveTo(x,y,z)
         parent[name]=ball
+        return parent[name]
     }
     async render(){
         await graphics.render(this.gl,this.prog,this.vbo,this.ibo,this.buffer)
+        
+        getDescendants(this.things).forEach(v=>{
+            v.update()
+        })
         requestAnimationFrame(this.render)
     }
     moveCam(x,y,z){
